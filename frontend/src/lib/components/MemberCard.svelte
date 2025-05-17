5b840a3e146e72130dd96f9bcbe568d88483612b
ATGLopez <111474174+ATGLopez@users.noreply.github.com>
2025-05-17 20:16:06 +0800

    team page draft

---
+++ frontend/src/lib/components/MemberCard.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+import GithubIcon from '$lib/assets/github.svg';
+
+export let name: string = ''
+export let username: string = ''
+
+</script>
+
+<main>
+  <div class="grid lg:grid-rows-[auto_auto_auto] gap-1">
+    <div class="w-40 h-40 border border-gray-800 rounded-sm">
+      <img src={`https://github.com/${username}.png`} alt="GitHub Avatar" class="object-cover rounded-sm w-full h-full" />
+    </div>
+
+    <p class="font-bold text-lg text-center">{name}</p>
+
+    <a href="https://github.com/{username}" target="_blank" class="flex justify-center items-center gap-2 text-lg">
+      <img src={GithubIcon} class="w-5 fill-[#F9D65E] hover:fill-[#FCE588]" alt="Github icon"/>
+      <p class="underline hover:no-underline">{username}</p>
+    </a>
+  </div>
+</main>
--- frontend/src/routes/team/+page.svelte
+++ frontend/src/routes/team/+page.svelte
@@ -1,1 +1,16 @@
-<h1>Team page</h1>
+<script>
+  import MemberCard from "$lib/components/MemberCard.svelte";
+</script>
+
+<main>
+  <div class="py-12 text-4xl text-center">
+    <b>Team</b>
+  </div>
+
+  <div class="grid grid-cols-1 md:grid-cols-3 gap-16 items-center">
+    <MemberCard name="Aneko Delfin" username="EkoChamber"/>
+    <MemberCard name="Alec Lopez" username="ATGLopez"/>
+    <MemberCard name="Juni Maca" username="junimaca"/>
+  </div>
+</main>  
+  
