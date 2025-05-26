98906f76ddd9c5afd943970bf9c1618d96aa95c5
ATGLopez <111474174+ATGLopez@users.noreply.github.com>
2025-05-26 17:11:23 +0800

    updated descriptions for healthy and cordana

--- frontend/src/lib/classification.ts
+++ frontend/src/lib/classification.ts
@@ -6,12 +6,15 @@ export const labelMap: Record<string, string> = {
 };
 
 export const descMap: Record<string, string> = {
-  healthy: "Healthy",
+  healthy: 
+    "Your banana leaf is healthy!",
   sigatoka: 
     "Sigatoka leaf spot disease refers to a fungal infection of banana leaves, leading to reduced photosynthesis and yield. " + 
     "It has two forms: yellow Sigatoka, which causes yellowing and browning of leaves, and black Sigatoka, characterized by dark streaks and lesions.",
   pestalotiopsis: 
     "Pestalotiopsis leaf blight is a common fungal disease that affects various plant species, including banana plants. " +
     "It infects the plant's leaves, leading to brown, water-soaked lesions on the leaf surface.",
-  cordana: "Cordana"
+  cordana: 
+    "Cordana leaf spot is a fungal infection that affects banana leaves, manifesting in pale brown diamond-shaped spots with yellow borders. " +
+    "These spots can often merge, which turn the cause large areas of the leaf to turn brown and dry out."
 };
